f5802c14aa30fd8f65c526617978c51c1218a3ec
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-12-06 00:12:51 +0900

    fix banner position

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_search/search_bar.dart';
 
+// バナー広告の高さ
+double adBannerHeight;
+
 void main() {
   runApp(MyApp());
 }
@@ -16,6 +19,27 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: SearchAppBar(),
+      builder: (BuildContext context, Widget child) {
+        // 画面サイズを取得(幅、高)
+        final Size screenSize = MediaQuery.of(context).size;
+
+        // 画面の高さ(dp)によって表示広告(スマートバナー)の高さをセット
+        if (screenSize.height > 720) {
+          adBannerHeight = 90.0;
+        } else if (screenSize.height > 400) {
+          adBannerHeight = 50.0;
+        } else {
+          adBannerHeight = 32.0;
+        }
+
+        // 下部にAdMob広告を表示するため、スペースを空ける
+        return Padding(
+          child: child,
+          padding: EdgeInsets.only(
+            bottom: adBannerHeight,
+          ),
+        );
+      },
     );
   }
 }
